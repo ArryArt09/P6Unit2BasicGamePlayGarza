3d640e147c52a300f10faa7308f20a8afcbc5662
ArryArt09 <746785@student.dusd.net>
2026-03-05 13:50:17 -0800

    Simple Start
    Try programing without the tutorials, it'll help when you need to program. Other than that, just keep adding features and you'll be good... at 2.2)

---
+++ P6GarzaUnit2GamePlay/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    public float zRange = 10;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keep the chnage in place x
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        // Keep the chnage in place z
+        if (transform.position.z < -2)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -2);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+    }
+}
