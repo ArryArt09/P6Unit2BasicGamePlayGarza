3cb8caf7b6033a2da92586131f10c248b59f2769
ArryArt09 <746785@student.dusd.net>
2026-03-13 13:52:03 -0700

    Almost Done
    work out issues then the bonuses, then you're free

--- P6GarzaUnit2GamePlay/Assets/Scripts/SpawnManager.cs
+++ P6GarzaUnit2GamePlay/Assets/Scripts/SpawnManager.cs
@@ -4,17 +4,36 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+        }
+        */
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //Random animal index and spawn pos
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
     }
 }
